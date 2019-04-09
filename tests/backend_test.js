1c529a560ad12d898a16ab299742951f4ce86640
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-09 15:29:26 +0300

    Added supertests and tape tests Relates #9

---
+++ tests/backend_test.js
@@ -0,0 +1,37 @@
+const tape = require('tape');
+const supertest = require('supertest');
+const router = require('../src/router');
+
+tape("Tape is working", t => {
+  t.equals(1, 1, "one equals one");
+  t.end();
+});
+
+tape('Home route returns a status code of 200', (t) => {
+    supertest(router)
+        .get("/")
+        .expect(200)
+        .expect('Content-Type', /html/)
+        .end((err, res) => {
+            t.error(err);
+
+      t.equal(res.statusCode, 200, 'Should return 200');
+      t.end()
+
+    });
+});
+
+
+tape('unkown route returns a status code of 404', (t) => {
+    supertest(router)
+        .get("/sdksd")
+        .expect(404)
+        .expect('Content-Type', /html/)
+        .end((err, res) => {
+            t.error(err);
+
+      t.equal(res.statusCode, 404, 'Should return 404');
+      t.end()
+
+    });
+});
