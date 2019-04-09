dd00b006d2b37ca7d49ecf69ead104b90138fac8
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-09 17:34:03 +0300

    Changed POST to GET in reading data

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const router = require('./router');
 const server = http.createServer(router);
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4010;
 
 server.listen(port, () => {
   console.log(`Server started at ${port}`);
