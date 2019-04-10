bba048c79fce4e7caa337e5750c0a156e72093ff
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-10 12:25:35 +0300

    Called the build function when this file is called

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -5,4 +5,6 @@ const connection = require('./db_connection');
 const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 const runDbBuild = cb => connection.query(sql,cb);
 
+runDbBuild();
+
 module.exports = runDbBuild;
