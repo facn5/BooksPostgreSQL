d5af81011cdcc71e2dcc95860b0c25bc2cc92a65
obaydah <obayda7h@gmail.com>
2019-04-10 12:37:47 +0300

    typo fixed

--- src/queries/postData.js
+++ src/queries/postData.js
@@ -1,12 +1,12 @@
 const dbConnection = require('../database/db_connection.js');
 
-const postData = (name, author, year, shortDesc, resp, cb) => {
+const postData = (name, author, year, shortDesc, response, cb) => {
   dbConnection.query(
     'INSERT INTO books (name, author, year, shortDesc) VALUES ($1, $2, $3, $4)',
     [name, author, year, shortDesc],
     (err, res) => {
       if (err) return cb(err);
-      cb(null, res, resp);
+      cb(null, res, response);
     }
   );
 };
