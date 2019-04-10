7f8b0c79afed24275bc67f2fc0519e320d80debf
karam1ashqar <karam.ashqar01@gmail.com>
2019-04-10 11:04:37 +0300

    Added another column in the books table

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -6,13 +6,14 @@ CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     author VARCHAR(100) NOT NULL,
-    year INTEGER
+    year INTEGER,
+    shortDesc VARCHAR(1000) NOT NULL
 );
 
-INSERT INTO books (name, author, year) VALUES 
-    ('Linux is a nutshell','AbuSalma',2009),
-    ('Linux is a not nutshell','Abu-Salma',2011),
-    ('Linux is a nothing','AbuSalma1',1999),
-    ('Windows is a nutshell','Abu',1965);
+INSERT INTO books (name, author, year, shortDesc) VALUES
+    ('Linux is a nutshell','AbuSalma',2009, 'a book describing the whole abu salma story'),
+    ('Linux is a not nutshell','Abu-Salma',2011, 'a book describing the whole abu salma story'),
+    ('Linux is a nothing','AbuSalma1',1999, 'a book describing the whole abu salma story'),
+    ('Windows is a nutshell','Abu',1965, 'a book describing the whole abu salma story');
 
 COMMIT;
